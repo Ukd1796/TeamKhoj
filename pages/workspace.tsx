c254b0786c4d282cd4bf5012b72303a6bd98ad46
dhairyaameria <dhairya.a@somaiya.edu>
2023-03-24 12:22:51 +0530

    card component

--- pages/workspace.tsx
+++ pages/workspace.tsx
@@ -2,6 +2,7 @@ import React, {useEffect,useState} from 'react';
 import { useUser} from '@auth0/nextjs-auth0';
 import {RoughNotation} from "react-rough-notation";
 import SideNavbar from "../components/SideNavbar";
+import Card from "../components/Card";
 import Layout from './layout';
 import Head from 'next/head';
 
@@ -10,6 +11,10 @@ export default function workspace(){
     return (
         <Layout className="bg-orange-100">
           <SideNavbar />
+          <div className="flex flex-col gradient h-screen items-center justify-center">
+          <Card />
+          </div>
+          
         </Layout>
       );
 }
